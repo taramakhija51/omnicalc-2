438cdc6cc6f29035a2f9afc0926d26ebd4ea1d0c
taramakhija51 <tmakhija@uchicago.edu>
2022-10-25 16:14:57 +0000

    first task complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ :template => "wizard/add.html.erb"})
+  end
 
+  def subtract
+    render({ :template => "wizard/subtract.html.erb"})
+  end
+
+  def divide
+    render({ :template => "wizard/divide.html.erb"})
+  end
+
+  def multiply
+    render({ :template => "wizard/multiply.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-
+  get("/wizard_add", {:controller => "application", :action => "add"})
+  get("/wizard_subtract", {:controller => "application", :action => "subtract"})
+  get("/wizard_multiply", {:controller => "application", :action => "multiply"})
+  get("/wizard_divide", {:controller => "application", :action => "divide"})
+  get("/", {:controller => "application", :action => "homepage"})
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
